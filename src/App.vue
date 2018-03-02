8c0fcea6b534b60fcb176602dfbde5ae6c5daa0f
Yohan Mallet <yoma@netc.fr>
2018-03-02 14:27:36 +0100

    modif App.vue : one immage & two toggle buttons to simulate active/inactive coffeemachines

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,16 @@
+
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+
+    <img src="./assets/coffee-bean.jpg">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+
+    <h2> Coffee Machine One </h2>
+    <toggle-button :value="true" :labels="{checked: 'Active', unchecked: 'inactive'}" width="70"/>
+
+    <h2> Coffee Machine Two </h2>
+    <toggle-button :value="false" :labels="{checked: 'Active', unchecked: 'inactive'}" width="70"/>
+
   </div>
 </template>
 
@@ -24,7 +19,7 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Coffee Machine App'
     }
   }
 }
